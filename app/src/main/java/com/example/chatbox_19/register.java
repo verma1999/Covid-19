5478368ed27dab5d4fc4b8037a7968aaa57ffbf8
mishra0007 <iammishrahere@gmail.com>
2020-05-05 01:56:59 +0530

    Just the Autantication

---
+++ app/src/main/java/com/example/chatbox_19/register.java
@@ -0,0 +1,86 @@
+package com.example.chatbox_19;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class register extends AppCompatActivity {
+    EditText mName, mEmail, mPassword, mPhone;
+    Button mRegister;
+    FirebaseAuth mAuth;
+    TextView mLogin;
+    ProgressBar mProgressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mName = findViewById(R.id.etName);
+        mEmail = findViewById(R.id.gvEmail);
+        mPassword = findViewById(R.id.gvPassword);
+        mPhone = findViewById(R.id.etPhone);
+        mLogin = findViewById(R.id.goToLogin);
+        mRegister = findViewById(R.id.register);
+        mProgressBar = findViewById(R.id.indeterminateBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length()<6){
+                    mPassword.setError("Password must be upto 6 character long");
+                    return;
+                }
+                mProgressBar.setVisibility(View.VISIBLE);
+                //register the user in fire base
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
